927bb9fd9bd7bce30fd5db055ce4a7247e98fced
MaxCazares <maxocazares@hotmail.com>
2022-02-04 14:39:49 -0600

    Primeros custom hooks

---
+++ hooks/useCounter.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+const useCounter = (initialCounter = 10) => {
+   
+    const [counter, setCounter] = useState(initialCounter);
+
+    const increment = () => {
+        setCounter(counter + 1);
+
+    }
+    const decrement = () => {
+        setCounter(counter - 1);
+    }
+
+    const reset = () => {
+        setCounter(initialCounter);
+    }
+    
+    return{
+        counter,
+        increment,
+        decrement,
+        reset,
+    }
+}
+ 
+export default useCounter;
